6e2e9def5e5e6562aafe38a1e6b9f6f36554bdaf
nerokingzz <61302720+nerokingzz@users.noreply.github.com>
2020-02-21 14:50:17 +0900

    Update gitTest2.java

--- src/gitTest2/gitTest2.java
+++ src/gitTest2/gitTest2.java
@@ -5,7 +5,7 @@ public class gitTest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("테스트2");
+		System.out.println("테스트2 454545");
 
 	}
 
